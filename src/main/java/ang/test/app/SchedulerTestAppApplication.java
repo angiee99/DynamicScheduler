0c4e3f284a2ed755384efd710376b87fbfe3ec5a
Anhelina Kulkova <angelinkulk@gmail.com>
2025-06-13 14:01:58 +0200

    Initial Setup

---
+++ src/main/java/ang/test/app/SchedulerTestAppApplication.java
@@ -0,0 +1,13 @@
+package ang.test.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerTestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ang/test/app/SchedulerTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ang.test.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
